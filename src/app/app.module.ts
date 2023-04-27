033f87e2a8d4dc54c70f85746d0aa6f07f69a361
steph_cs <stecampelo@gmail.com>
2023-04-27 12:25:07 -0300

    project config + dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardSmComponent } from './card-sm/card-sm.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/*Components*/
+import { CardsListComponent } from './cards-list/cards-list.component';
+import { CardLgComponent } from './card-lg/card-lg.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
+/*Modules*/
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardSmComponent,
+    CardsListComponent,
+    CardLgComponent,
+    NavbarComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
